a3512ad5b42058c881d5fa0328b13b5be196e678
4SAMU <samuelnzomo4@gmail.com>
2023-07-18 20:48:22 +0300

    update

--- Components/Blogg/Styles.ts
+++ Components/Blogg/Styles.ts
@@ -20,6 +20,24 @@ export const BlogContent = styled(Box)({
   flexDirection: "row",
 });
 
+export const LineBetween = styled(Box)({
+  height: "40px",
+  width: "100%",
+  background: "none",
+  marginTop: "20px",
+  borderTop: "1px solid rgb(242, 242, 242)",
+  borderBottom: "1px solid rgb(242, 242, 242)",
+  display: "flex",
+  alignItems: "center",
+  flexDirection: "row",
+  gap: "30px",
+  "& .MinsRead": {
+    color: "rgb(107, 107, 107)",
+    fontFamily: "Roboto",
+    fontWeight: 200,
+  },
+});
+
 export const Content = styled(Box)({
   minHeight: "80vh",
   width: "70%",
@@ -97,9 +115,11 @@ export const RelatedArticles = styled(Box)({
 import { useState, useEffect } from "react";
 export const RelatedCard = styled(Box)<ImageProps>(({ imageUrl }) => {
   const [isDarkBackground, setIsDarkBackground] = useState(false);
+  const [imageLoadingError, setImageLoadingError] = useState(false);
 
   useEffect(() => {
     const img = new Image();
+    console.log(img);
     img.crossOrigin = "anonymous"; // Add the crossOrigin attribute
     img.src = imageUrl;
 
@@ -127,49 +147,65 @@ export const RelatedCard = styled(Box)<ImageProps>(({ imageUrl }) => {
       const averageBrightness = totalBrightness / (img.width * img.height);
       setIsDarkBackground(averageBrightness < 128);
     };
+    img.onerror = () => {
+      // Error loading the image
+      setImageLoadingError(true);
+    };
   }, [imageUrl]);
 
   return {
     position: "relative",
-    height: "200px",
+    height: "220px",
     width: "96%",
     marginLeft: "2%",
-    marginTop: "10px",
-    marginBottom: "10px",
+    marginTop: "15px",
+    marginBottom: "15px",
     boxShadow: "5px 0px 20px rgba(0, 0, 0, 0.3)",
     cursor: "pointer",
-    background: `url(${imageUrl})`,
+    background:
+      imageUrl && !imageLoadingError
+        ? `url(${imageUrl})`
+        : "rgb(242, 242, 242)",
     backgroundRepeat: "no-repeat",
     backgroundSize: "100% 100%",
+    borderRadius: "4px",
     "& .OvarlayColor": {
       position: "absolute",
       bottom: "0",
-      height: "40%",
+      minHeight: "30%",
+      height: "auto",
       width: "100%",
       background: isDarkBackground
         ? "rgba(255, 255, 255, 0.6)"
         : "rgba(0, 0, 0, 0.4)",
-      backdropFilter: "blur(5px)",
+      backdropFilter: "blur(3px)",
     },
     "& .cardContent": {
-      position: "absolute",
-      bottom: "0",
-      height: "40%",
-      width: "100%",
+      padding: "10px",
     },
     "& .title": {
-      fontSize: "16px",
-      fontFamily: "Open Sans",
-      fontWeight: "600",
-      margin: "10px 0 0 5%",
-      color: !isDarkBackground ? "#FFFFFF" : "#000000",
-    },
-    "& .overview": {
       fontSize: "14px",
       fontFamily: "Gotham",
-      fontWeight: "400",
-      margin: "8px 0 0 5%",
+      fontWeight: "600",
+      lineHeight: "18px",
       color: !isDarkBackground ? "#FFFFFF" : "#000000",
+      display: "-webkit-box",
+      "-webkit-line-clamp": "2",
+      "-webkit-box-orient": "vertical",
+      overflow: "hidden",
+    },
+    "& .overview": {
+      fontSize: "12px",
+      fontFamily: "sans-serif",
+      fontWeight: "400",
+      lineHeight: "14px",
+      letterSpacing: "0.3px",
+      margin: "8px 0 0 0",
+      color: !isDarkBackground ? "#FFFFFF" : "#000000",
+      display: "-webkit-box",
+      "-webkit-line-clamp": "2",
+      "-webkit-box-orient": "vertical",
+      overflow: "hidden",
     },
   };
 });
--- pages/Telastori/[blogg].tsx
+++ pages/Telastori/[blogg].tsx
@@ -10,7 +10,7 @@ import {
   BlogImage,
   AurthorCardImg,
   AuthorName,
-  CardImage,
+  LineBetween,
 } from "@/Components/Blogg/Styles";
 import Footer from "@/Components/Footer/Footer";
 import Navbar from "@/Components/Navbar";
@@ -28,13 +28,19 @@ const Blog = () => {
       <BlogContainer>
         <BlogContent>
           <Content>
+            <h1>
+              How I Eliminated Procrastination From My Life (Using Neuroscience)
+            </h1>
+            <LineBetween>
+              <div className="MinsRead">3 Mins Read</div>
+              <div className="MinsRead">.</div>
+              <div className="MinsRead">Apr 5, 2022</div>
+            </LineBetween>
             <BlogImage
               src="https://miro.medium.com/v2/resize:fit:720/0*xLIQQ3iLqGqz8LFz"
               alt="no found"
             />
-            <h1>
-              How I Eliminated Procrastination From My Life (Using Neuroscience)
-            </h1>
+
             <p>
               Procrastination is a big problem for many of us. We say we’ll work
               on our goals, hit the gym, or learn some new skills, only to end
@@ -149,25 +155,57 @@ const Blog = () => {
           </Aurthor>
           <RelatedArticles>
             <h1>Related Articles</h1>
-            <RelatedCard imageUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTxTPQbQIWrVhXPiD9V0I7yp2HDEyw4STOpPw&usqp=CAU">
-              <div className="OvarlayColor" />
-              <div className="cardContent">
-                <div className="title">Hello we have sea, come and see</div>
-                <div className="overview">Hello we have sea, come and see</div>
+            <RelatedCard imageUrl="https://cdn.sanity.io/images/taki18de/production/a9ad4cdd0220f5c42764ba74a4598caef008328a-850x474.webp">
+              <div className="OvarlayColor">
+                <div className="cardContent">
+                  <div className="title">
+                    Swap a €30k Budget for Just 5 Days Worth of Dev Time:
+                    Relaunching Fresco Cooks.
+                  </div>
+                  <div className="overview">
+                    Fresco Cooks is a B2B SaaS brand that powers seamless
+                    cooking experiences for the world's leading appliance brands
+                    and home cooks. They originally came to Mawla in 2022
+                    because they needed a new marketing site as part of their
+                    rebrand to reflect the company’s growth.s
+                  </div>
+                </div>
               </div>
             </RelatedCard>
-            <RelatedCard imageUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMxD6L-qxl8OH1hZnuYS4ImZMh2ldM0BH4Sg&usqp=CAU">
-              <div className="OvarlayColor" />
-              <div className="cardContent">
-                <div className="title">Hello we have sea, come and see</div>
-                <div className="overview">Hello we have sea, come and see</div>
+            <RelatedCard imageUrl="https://miro.medium.com/v2/resize:fit:720/0*dXTEWRECBHcbLgTZ">
+              <div className="OvarlayColor">
+                <div className="cardContent">
+                  <div className="title">
+                    23 passive income DApps that make money in your sleep [2023]
+                  </div>
+                  <div className="overview">
+                    Passive income is income that is generated with minimal
+                    effort or time. This type of income is typically generated
+                    through investments or other forms of passive
+                    income-generating activities such as renting out the
+                    property, creating digital products (e.g. online courses),
+                    and so on. Passive income is important to have multiple
+                    streams…
+                  </div>
+                </div>
               </div>
             </RelatedCard>
-            <RelatedCard imageUrl="/HomeImage.jpg">
-              <div className="OvarlayColor" />
-              <div className="cardContent">
-                <div className="title">Hello we have sea, come and see</div>
-                <div className="overview">Hello we have sea, come and see</div>
+            <RelatedCard imageUrl="https://miro.medium.com/v2/resize:fit:720/0*xLIQQ3iLqGqz8LFz">
+              <div className="OvarlayColor">
+                <div className="cardContent">
+                  <div className="title">
+                    How I Eliminated Procrastination From My Life (Using
+                    Neuroscience)
+                  </div>
+                  <div className="overview">
+                    Procrastination is a big problem for many of us. We say
+                    we’ll work on our goals, hit the gym, or learn some new
+                    skills, only to end up watching Netflix instead. I used to
+                    be a chronic procrastinator too. I’d set big goals and make
+                    ambitious plans, but not much was getting done on a
+                    day-to-day basis.
+                  </div>
+                </div>
               </div>
             </RelatedCard>
           </RelatedArticles>
